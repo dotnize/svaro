22e2a17148bccbf618cfc751a0eb240056df14c3
Nathaniel Tampus <nate@mugnavo.com>
2024-07-28 17:27:52 +0800

    reexport components & types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
-// Reexport your entry components here
+export { default as Canvas } from "./components/Canvas.svelte";
+export { default as ComponentList } from "./components/ComponentList.svelte";
+export { default as Svaro } from "./components/Svaro.svelte";
+
+export type * from "./types/config.js";
+export type * from "./types/fields.js";
